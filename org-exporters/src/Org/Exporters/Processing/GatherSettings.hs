1236b0b4ee77ca0372dcc9d4b5b71572e781aeab
Lucas V. R <lvr@s-viva.xyz>
2023-02-09 19:07:55 -0300

    rename: GatherKeywords -> GatherSettings

---
+++ org-exporters/src/Org/Exporters/Processing/GatherSettings.hs
@@ -0,0 +1,27 @@
+module Org.Exporters.Processing.GatherSettings where
+
+import Data.Text qualified as T
+import Org.Exporters.Processing.OrgData
+import Org.Types (KeywordValue (..), OrgDocument (..), OrgElement (..))
+import Org.Walk
+
+registerFiletags :: KeywordValue -> M ()
+registerFiletags = \case
+  ValueKeyword tags ->
+    modify2 \s ->
+      return s {filetags = filetags s ++ filter (not . T.null) (T.split (== ':') tags)}
+  _ -> pure ()
+
+getKeywordSetting :: OrgElement -> Ap M ()
+getKeywordSetting = \case
+  -- Keyword "exclude_tags" _ -> error "todo" TODO
+  -- Keyword "select_tags" _ -> error "todo"
+  -- Keyword "options" _ -> error "todo"
+  Keyword k v -> Ap do
+    case k of
+      "filetags" -> registerFiletags v
+      _ -> pure ()
+  _ -> pure ()
+
+gatherSettings :: OrgDocument -> M ()
+gatherSettings = getAp . query getKeywordSetting
