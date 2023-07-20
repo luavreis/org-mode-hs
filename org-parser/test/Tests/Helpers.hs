873238feddf246a830088cca3b5fb690e9232e3d
lucasvreis <lvreis@usp.br>
2023-07-20 16:53:41 -0300

    parser: organize test helpers
    almost useless changes at this point, but whatever

--- org-parser/test/Tests/Helpers.hs
+++ org-parser/test/Tests/Helpers.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Tests.Helpers
   ( module Tests.Helpers
@@ -10,41 +10,32 @@ module Tests.Helpers
   )
 where
 
-import Data.Algorithm.DiffContext (getContextDiff, prettyContextDiff)
+import Data.TreeDiff
 import Org.Builder (Many)
 import Org.Parser
 import Org.Parser.Objects (Marked (..))
-import Org.Types (OrgDocument, OrgElementData, Properties)
+import Org.Types
 import Test.Tasty
 import Test.Tasty.HUnit
 import Text.Megaparsec (eof)
 import Text.Megaparsec.Error (errorBundlePretty)
-import Text.Pretty.Simple
-import Text.PrettyPrint (render, text)
 
 {- | This class is mainly used for the tests cases.
 @Parsed m a@ is the "monad-stripped" version of parse
 tree with which we can compare in the test cases.
 -}
-class Parsable m a where
-  type Parsed m a
-  parse' :: m a -> Text -> Either OrgParseError (Parsed m a)
+class Parsable m where
+  parse' :: m a -> Text -> Either OrgParseError a
 
-instance Parsable OrgParser a where
-  type Parsed OrgParser a = a
+instance Parsable OrgParser where
   parse' p = parseOrg defaultOrgOptions (p <* eof) ""
 
-instance Parsable (Marked OrgParser) a where
-  type Parsed (Marked OrgParser) a = a
+instance Parsable (Marked OrgParser) where
   parse' p = parse' (getParser p)
 
 instance PrettyFormable Properties where
   type PrettyForm Properties = Properties
   prettyForm = id
-
-instance Parsable OrgParser OrgDocument where
-  type Parsed OrgParser OrgDocument = OrgDocument
-  parse' p = parseOrg defaultOrgOptions (p <* eof) ""
 
 instance PrettyFormable OrgDocument where
   type PrettyForm OrgDocument = OrgDocument
@@ -62,11 +53,11 @@ instance PrettyFormable OrgElementData where
   type PrettyForm OrgElementData = OrgElementData
   prettyForm = id
 
-prettyParse :: (Parsable m a, PrettyFormable (Parsed m a), Show (PrettyForm (Parsed m a))) => m a -> Text -> IO ()
+prettyParse :: (Parsable m, PrettyFormable a, ToExpr (PrettyForm a)) => m a -> Text -> IO ()
 prettyParse parser txt =
   case parse' parser txt of
     Left e -> putStrLn $ errorBundlePretty e
-    Right x -> pPrint $ prettyForm x
+    Right x -> print $ ansiWlBgExpr $ toExpr $ prettyForm x
 
 infix 1 =?>
 
@@ -87,10 +78,10 @@ infix 4 ~:
 
 (~:) ::
   HasCallStack =>
-  (Parsable m a, PrettyFormable (Parsed m a), Eq (Parsed m a), Show (Parsed m a)) =>
+  (Parsable m, PrettyFormable a, Eq a, ToExpr (PrettyForm a)) =>
   TestName ->
   m a ->
-  [(Text, Either () (Parsed m a))] ->
+  [(Text, Either () a)] ->
   TestTree
 (~:) name parser cases =
   testGroup name $
@@ -100,14 +91,43 @@ infix 4 ~:
           Left e
             | isRight ref -> assertFailure $ errorBundlePretty e
             | otherwise -> pure ()
-          Right x
-            | Right ref' <- ref ->
-                unless (x == ref') do
-                  let reflines = map toString $ lines (toStrict $ pShow ref')
-                      gotlines = map toString $ lines (toStrict $ pShow x)
-                      diff = getContextDiff 3 reflines gotlines
-                      pdiff = prettyContextDiff (text "Test reference") (text "Parsed") text diff
-                  assertFailure (render pdiff)
+          Right got
+            | Right reference <- ref ->
+                unless (got == reference) $
+                  assertFailure (diffExpr got reference)
             | otherwise ->
                 assertFailure $
-                  "Should not parse, but parsed as:\n" <> show x
+                  "Should not parse, but parsed as:\n" <> renderExpr got
+  where
+    renderExpr :: (PrettyFormable a, ToExpr (PrettyForm a)) => a -> String
+    renderExpr = show . ansiWlBgExpr . toExpr . prettyForm
+    diffExpr :: (PrettyFormable a, ToExpr (PrettyForm a)) => a -> a -> String
+    diffExpr a b = show $ ansiWlBgEditExpr $ ediff (toExpr $ prettyForm a) (toExpr $ prettyForm b)
+
+deriving instance (ToExpr OrgDocument)
+deriving instance (ToExpr KeywordValue)
+deriving instance (ToExpr OrgObject)
+deriving instance (ToExpr QuoteType)
+deriving instance (ToExpr TimestampData)
+deriving instance (ToExpr FragmentType)
+deriving instance (ToExpr FootnoteRefData)
+deriving instance (ToExpr Citation)
+deriving instance (ToExpr CiteReference)
+deriving instance (ToExpr BabelCall)
+deriving instance (ToExpr LinkTarget)
+deriving instance (ToExpr OrgElement)
+deriving instance (ToExpr OrgElementData)
+deriving instance (ToExpr GreaterBlockType)
+deriving instance (ToExpr ListType)
+deriving instance (ToExpr OrderedStyle)
+deriving instance (ToExpr ListItem)
+deriving instance (ToExpr Bullet)
+deriving instance (ToExpr Checkbox)
+deriving instance (ToExpr SrcLine)
+deriving instance (ToExpr TableRow)
+deriving instance (ToExpr ColumnAlignment)
+deriving instance (ToExpr OrgSection)
+deriving instance (ToExpr TodoKeyword)
+deriving instance (ToExpr TodoState)
+deriving instance (ToExpr Priority)
+deriving instance (ToExpr PlanningInfo)
